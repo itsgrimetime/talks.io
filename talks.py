e0403d05849620427661dbf8fd40f80d1cb9bbb0
Mike Grimes <magrimes@mtu.edu>
2013-11-05 17:58:57 -0500

    add before_request and on_teardown callbacks

--- talks.py
+++ talks.py
@@ -1,13 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+	db.close()
+
 @app.route("/")
 def homepage():
     return "Talks Homepage!"
 
 @app.route("/video/<int:video_id>")
 def show_video(video_id):
-    return "Video %d" % video_id
+    # gotta get video from database
+
+    # then we can just display its attributes
+    return """ Video {id} by {author}
+		{link} """.format(id=video_id)
 
 if __name__ == "__main__":
     app.run()
