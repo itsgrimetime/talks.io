7a641bca834a183e71043a7fe67689d918c4e250
Mike Grimes <magrimes@mtu.edu>
2013-11-05 17:47:57 -0500

    add basic video class

---
+++ video.py
@@ -0,0 +1,11 @@
+
+class Video():
+
+    def __init__(self, link, author, submitter):
+	self.link = link
+	self.ratings = []
+	self.rating = 0.0
+	self.comments = []
+	self.tags = []
+	self.submitter = None
+	self.author = None
